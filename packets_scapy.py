52cde75888550ffa73b93336a6c3b632d5868a5c
WisdomDaPhoenix <hotwizzytron@gmail.com>
2025-08-23 11:05:29 +0100

    New Image files

--- packets_scapy.py
+++ packets_scapy.py
@@ -1,8 +1,20 @@
 from scapy.all import IP, ICMP, send
 
 
-# packet = IP(dst="google.com")
-packet = IP(dst='192.168.56.105')/ICMP()
+packet = IP(dst="google.com")/ICMP()
+
 print(packet.summary())
 send(packet)
 
+
+
+
+
+
+
+
+
+
+
+
+#packet = IP(dst='192.168.56.105')/ICMP()
